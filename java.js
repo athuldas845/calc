a5778db7a731b85c37b85c3439a066775749c711
athuldas845 <154975120+athuldas845@users.noreply.github.com>
2025-06-16 20:02:47 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,95 +1,113 @@
-var op, num = "", flag = 0, power = 0;
-const clicksound = new Audio("clicksound.mp3");
-const onsound = new Audio("ui.mp3");
-function start() {
-    if (power == 0) {
-        onsound.play();
-        document.res.in_num2.value = "::::::::::::::::::CASIO:::::::::::::::::::::::";
-        setTimeout(() => {
-            document.res.in_num2.value = "";
-        }, 2000);
-    }
-    power = 1;
-}
-function stop() {
-    clearinput();
-    if (power == 1) {
-        document.res.in_num2.value = "::::::::::::::Bye::::::::::::::::::::";
-        setTimeout(() => {
-            document.res.in_num2.value = "";
-        }, 2000);
-    }
-    power = 0;
-}
-function clearinput() {
-    clicksound.play();
-    document.res.in_num1.value = "";
-    document.res.in_num2.value = "";
-    document.res.result.value = "";
-    document.res.sym.value = "";
-    num = "";
-    enter('n');
-}
-function enter(ch) {
-    clicksound.play();
-    if (ch == 'c') {
-        if (flag == 0) {
-            flag = 1;
-            num = "";
+        let inputs = [];
+        let num = [];
+        let sign = [];
+        var i = 0, numlength = 0, signlength = 0, power = 0;
+        const clicksound = new Audio("clicksound.mp3");
+        const onsound = new Audio("ui.mp3");
+        function start() {
+            if (power == 0) {
+                onsound.play();
+                document.res.in_num1.value = "       WELCOME";
+                setTimeout(() => {
+                    document.res.in_num1.value = "";
+                }, 2000);
+            }
+            power = 1;
         }
-        else {
-            flag = 0;
-            num = "";
+        function stop() {
+            clearinput();
+            if (power == 1) {
+                document.res.in_num1.value = "    SEE U SOON";
+                setTimeout(() => {
+                    document.res.in_num1.value = "";
+                }, 2000);
+            }
+            power = 0;
         }
-    }
-    else {
-        if (flag == 1) {
-            flag = 0;
-            num = "";
+        function clearinput() {
+            clicksound.play();
+            document.res.in_num1.value = "";
+            document.res.result.value = "";
+            inputs = [];
+            num = [];
+            sign = [];
+            i=0; numlength = 0; signlength = 0;
         }
-        num = "";
-    }
-}
-function input(ch) {
-    clicksound.play();
-    if (power == 1) {
-        num += document.res[ch].value;
-        if (flag == 0) {
-            document.res.in_num1.value = num;
+        function remove() {
+            i--;
+            inputs[i] = "";
+            displayoninputbox();
         }
-        else { document.res.in_num2.value = num; }
-    }
-}
-function sign(x) {
-    clicksound.play();
-    if (power == 1) {
-        document.res.sym.value = x;
-        enter('c');
-    }
-}
-function output() {
-    if (power == 1) {
-        var ch, num1, num2;
-        num1 = Number(document.res.in_num1.value);
-        num2 = Number(document.res.in_num2.value);
-        ch = document.res.sym.value;
-        switch (ch) {
-            case '+':
-                op = num1 + num2;
-                break;
-            case '-':
-                op = num1 - num2;
-                break;
-            case 'x':
-                op = num1 * num2;
-                break;
-            case '%':
-                op = num1 / num2;
-                break;
-            default:
-                op = "SOMETHING MISSING!!"
+        function input(ch) {
+            clicksound.play();
+            if (power == 1) {
+                inputs[i] = document.res[ch].value;
+                i++;
+                displayoninputbox();
+            }
         }
-        document.res.result.value = op;
-        enter('n');
-    }
-}
+        function displayoninputbox() {
+            var display = "", j = 0;
+            while (j < i) {
+                display += inputs[j];
+                j++;
+            }
+            document.res.in_num1.value = display;
+        }
+        function split() {///split the inputs array to num array & sign array
+            var index = 0, lastindex = 0;
+            while (index < inputs.length) {
+                num[numlength] = "";
+                if (inputs[index] == '+' || inputs[index] == '-' || inputs[index] == 'x' || inputs[index] == '%') {
+                    while (lastindex < index) {//concatenating numbers up to th sign(before the sign)
+                        num[numlength] += inputs[lastindex];
+                        lastindex++;
+                    }
+                    sign[signlength] = inputs[lastindex];//storing sign
+                    lastindex++; numlength++; signlength++;
+                }
+                index++;
+            }
+            if (lastindex < inputs.length) {//concatenating the last numbers (afer the sign)
+                while (lastindex < inputs.length) {
+                    num[numlength] += inputs[lastindex];
+                    lastindex++;
+                }
+            }
+        }
+
+        function resultant() {
+            clicksound.play();
+            if (power == 1) {
+                var lastanswer = 0, firstnumindex = 0, secondnumindex = 1, currentsign, indexofsign = 0, num1, num2;
+                split();
+                lastanswer = Number(num[firstnumindex]);
+                while (secondnumindex < num.length) {
+                    num1 = lastanswer;
+                    num2 = Number(num[secondnumindex]);
+                    currentsign = sign[indexofsign];
+                    switch (currentsign) {
+                        case '+':
+                            lastanswer = num1 + num2;
+                            break;
+                        case '-':
+                            lastanswer = num1 - num2;
+                            break;
+                        case 'x':
+                            lastanswer = num1 * num2;
+                            break;
+                        case '%':
+                            lastanswer = num1 / num2;
+                            break;
+                        default:
+                            lastanswer = "SOMETHING MISSING!!"
+                    }
+                    secondnumindex++;
+                    indexofsign++;
+                }
+                document.res.result.value = lastanswer;
+                num = [];
+                sign = [];
+                numlength = 0; signlength = 0;
+            }
+        }
